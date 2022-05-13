257b047319e5e1b4d6b9420c313bdb2aa0adb918
smhrd <smhrd@211.227.224.244>
2022-05-13 17:48:36 +0900

    준섭

--- Jeju_maven/src/main/java/com/smhrd/domain/MemberDAO.java
+++ Jeju_maven/src/main/java/com/smhrd/domain/MemberDAO.java
@@ -1,16 +1,85 @@
 package com.smhrd.domain;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionManager;
 
-
+import com.smhrd.database.SqlSessionManager;
 
 public class MemberDAO {
+   // SqlSessionFactiory 생성 (연결해야하는 DB 설정 정보 가지고 있음)
+   // -> Session 생성 (DB 연결, 작업수행, 종료 작업)
+   SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
+   
 
-		// SqlSessionFactory 생성(연결해야하는 db설정 정보 가지고있음)
-		// ==> Session 생성(db연결, 작업수행, 종료 작업)
-		
-		
-		
-
+   // 회원가입 기능
+   public int insertMember(Member member) {
+	   SqlSession sqlSession = sqlSessionFactory.openSession();
+      int cnt = 0;
+      try {
+         cnt = sqlSession.insert("com.smhrd.domain.MemberDAO.insertMember", member);
+         if (cnt > 0) {
+            sqlSession.commit();
+         } else {
+            sqlSession.rollback();
+         }
+      } catch (Exception e) {
+         e.printStackTrace();
+      } finally {
+         sqlSession.close();
+      }
+      return cnt;
+   }
+   
+   //로그인기능
+   public Member selectMember(Member member) {
+	   SqlSession sqlSession = sqlSessionFactory.openSession();
+	   Member loginMember = null;
+	   try {
+		   //selectOne() -> 결과값(Object)
+		   // -> 결과값이 항상 1개 아니면 null
+		   //같은 아이디/패스워드가 테이블에 여러개 들어가 있을 경우에는 오류!
+		   	 loginMember = sqlSession.selectOne("com.smhrd.domain.MemberDAO.selectMember", member);
+	         //cnt = sqlSession.insert("com.smhrd.domain.MemberDAO.insertMember", member);
+	         if (loginMember != null) {
+	            sqlSession.commit();
+	         } else {
+	            sqlSession.rollback();
+	         }
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	      } finally {
+	         sqlSession.close();
+	      }
+	      return loginMember;
+	   }
+   		
+   	
+   		
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
