3667d4527ea16361eeae4003c65e0471b220764f
smhrd <smhrd@211.228.36.206>
2022-05-13 16:45:17 +0900

    희연 - dao, vo커밋

---
+++ Jeju_maven/src/main/java/com/smhrd/domain/MemberDAO.java
@@ -0,0 +1,15 @@
+package com.smhrd.domain;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+
+
+
+public class MemberDAO {
+
+		// SqlSessionFactory 생성(연결해야하는 db설정 정보 가지고있음)
+		// ==> Session 생성(db연결, 작업수행, 종료 작업)
+		
+		
+		
+
+}
