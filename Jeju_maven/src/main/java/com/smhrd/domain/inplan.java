4842c32fd2a175ea5c1996c1cf05f4ee83c54330
smhrd <smhrd@121.147.52.64>
2022-05-17 11:08:37 +0900

    저장용

--- Jeju_maven/src/main/java/com/smhrd/domain/inplan.java
+++ Jeju_maven/src/main/java/com/smhrd/domain/inplan.java
@@ -9,4 +9,44 @@ public class inplan {
 	private String plan_pen;
 	private String mem_id;
 	
+	public int getInplan_num() {
+		return inplan_num;
+	}
+
+	public String getInplan_name() {
+		return inplan_name;
+	}
+
+	public String getInplan_date() {
+		return inplan_date;
+	}
+
+	public String getInplan_time() {
+		return inplan_time;
+	}
+
+	public String getInplan_tour() {
+		return inplan_tour;
+	}
+
+	public String getPlan_pen() {
+		return plan_pen;
+	}
+
+	public String getMem_id() {
+		return mem_id;
+	}
+
+	public inplan(int inplan_num, String inplan_name, String inplan_date, String inplan_time, String inplan_tour,
+			String plan_pen, String mem_id) {
+		super();
+		this.inplan_num = inplan_num;
+		this.inplan_name = inplan_name;
+		this.inplan_date = inplan_date;
+		this.inplan_time = inplan_time;
+		this.inplan_tour = inplan_tour;
+		this.plan_pen = plan_pen;
+		this.mem_id = mem_id;
+	}
+	
 }
