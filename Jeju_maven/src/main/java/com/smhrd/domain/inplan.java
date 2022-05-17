177470956d0c077f04185d00406f49dc7e8f9970
smhrd <smhrd@121.147.52.64>
2022-05-17 11:07:47 +0900

    대기

---
+++ Jeju_maven/src/main/java/com/smhrd/domain/inplan.java
@@ -0,0 +1,12 @@
+package com.smhrd.domain;
+
+public class inplan {
+	private int inplan_num;
+	private String inplan_name;
+	private String inplan_date;
+	private String inplan_time;
+	private String inplan_tour;
+	private String plan_pen;
+	private String mem_id;
+	
+}
---
+++ Jeju_maven/src/main/java/com/smhrd/domain/inplanDAO.java
@@ -0,0 +1,9 @@
+package com.smhrd.domain;
+
+public class inplanDAO {	
+	
+	
+	
+	
+
+}
