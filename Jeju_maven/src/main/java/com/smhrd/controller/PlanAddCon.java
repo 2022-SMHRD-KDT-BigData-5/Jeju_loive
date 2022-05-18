061e51eab6008747d87e2fe8182dfe87fca4bd64
smhrd <smhrd@211.228.36.206>
2022-05-18 18:06:20 +0900

    희연 plan커밋

--- Jeju_maven/src/main/java/com/smhrd/controller/PlanAddCon.java
+++ Jeju_maven/src/main/java/com/smhrd/controller/PlanAddCon.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.smhrd.domain.Member;
+import com.smhrd.domain.inplan;
+import com.smhrd.domain.inplanDAO;
 import com.smhrd.domain.tour;
 
 
@@ -20,19 +22,31 @@ public class PlanAddCon extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		request.setCharacterEncoding("UTF-8");
+		//값 받아오기
 		int tourNum = Integer.parseInt(request.getParameter("tourNum"));
-		
+		//tourNum에 맞는 tour_name 가져오기
 		HttpSession session = request.getSession();
+		session.getAttribute("tourInfo");
+		
+		
+		
+		
 		Member loginMember = (Member)session.getAttribute("loginMember");
+		String mem_id = loginMember.getId();
 		PrintWriter out = response.getWriter();
+		inplanDAO dao = new inplanDAO();
+		inplan inplan = new inplan(plan_num, inplan_name, tour_num, mem_id);
 		
 		if(loginMember==null) {
+			//로그인 하지 않은 경우
 			System.out.println("로그인이 필요한 작업입니다.");
-			out.print("로그인이 필요합니다.");
+			response.sendRedirect("Join.jsp");
+			
 		}else {
 			//로그인을 한 경우
 			//플래너에 일정 추가하기
 			
+			dao.insertPlan(inplan);
 			
 			
 			
--- Jeju_maven/src/main/java/com/smhrd/domain/inplan.java
+++ Jeju_maven/src/main/java/com/smhrd/domain/inplan.java
@@ -6,22 +6,16 @@ import java.sql.Timestamp;
 public class inplan {
 	private BigDecimal inplan_num;
 	private BigDecimal plan_num;
-	private BigDecimal inplan_order;
 	private String inplan_name;
 	private BigDecimal tour_num;
 	private String mem_id;
 	
 	
-	
-	//getter
 	public BigDecimal getInplan_num() {
 		return inplan_num;
 	}
 	public BigDecimal getPlan_num() {
 		return plan_num;
-	}
-	public BigDecimal getInplan_order() {
-		return inplan_order;
 	}
 	public String getInplan_name() {
 		return inplan_name;
@@ -33,20 +27,22 @@ public class inplan {
 		return mem_id;
 	}
 	
-	
-	
-	//생성자
-	public inplan(BigDecimal inplan_num, BigDecimal plan_num, BigDecimal inplan_order, String inplan_name,
-			BigDecimal tour_num, String mem_id) {
+	public inplan(BigDecimal inplan_num, BigDecimal plan_num, String inplan_name, BigDecimal tour_num, String mem_id) {
 		super();
 		this.inplan_num = inplan_num;
 		this.plan_num = plan_num;
-		this.inplan_order = inplan_order;
 		this.inplan_name = inplan_name;
 		this.tour_num = tour_num;
 		this.mem_id = mem_id;
 	}
 	
+	public inplan(BigDecimal plan_num, String inplan_name, BigDecimal tour_num, String mem_id) {
+		super();
+		this.plan_num = plan_num;
+		this.inplan_name = inplan_name;
+		this.tour_num = tour_num;
+		this.mem_id = mem_id;
+	}
 	
 	
 	
