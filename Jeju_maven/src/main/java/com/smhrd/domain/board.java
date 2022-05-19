eea930da0462a9a36bd83d7843f0ed756fe1db88
smhrd <smhrd@211.227.224.244>
2022-05-19 16:52:32 +0900

    준섭커밋

--- Jeju_maven/src/main/java/com/smhrd/domain/board.java
+++
@@ -1,9 +0,0 @@
-package com.smhrd.domain;
-
-import java.math.BigDecimal;
-
-public class board {
-	BigDecimal bor_num;
-	String bor_name;
-	
-}
--- Jeju_maven/src/main/java/com/smhrd/domain/boardDAO.java
+++
@@ -1,5 +0,0 @@
-package com.smhrd.domain;
-
-public class boardDAO {
-	
-}
